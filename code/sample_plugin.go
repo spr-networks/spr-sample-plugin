817c353b85a45f2123f81fa7aa85c736d21005d8
lts-po <po@longterm.io>
2024-01-09 14:01:33 +0100

    add /ui for static html

--- code/sample_plugin.go
+++ code/sample_plugin.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 import (
@@ -39,9 +40,35 @@ func busListener() {
 
 var UNIX_PLUGIN_LISTENER = "/state/plugins/sample_plugin/socket"
 
+// set up SPA handler. From gorilla mux's documentation
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	path = filepath.Join(h.staticPath, path)
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
 
 func pluginTest(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "Not implemented", 400)
+	//http.Error(w, "Not implemented", 400)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "\"hello world\"")
 }
 
 func logRequest(handler http.Handler) http.Handler {
@@ -55,6 +82,10 @@ func main() {
 	unix_plugin_router := mux.NewRouter().StrictSlash(true)
 
 	unix_plugin_router.HandleFunc("/test", pluginTest).Methods("GET")
+
+	// map /ui to /ui on fs
+	spa := spaHandler{staticPath: "/ui", indexPath: "index.html"}
+	unix_plugin_router.PathPrefix("/ui").Handler(spa)
 
 	os.Remove(UNIX_PLUGIN_LISTENER)
 	unixPluginListener, err := net.Listen("unix", UNIX_PLUGIN_LISTENER)
